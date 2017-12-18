d0f29209e0819212d406deed6b5f5ab329d4feff
Peter Wasonga Ombwa <pwombwa@live.com>
2017-12-18 14:12:42 +0000

    First Commit

---
+++ PowerRecursive/PowerRecursive/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerRecursive
+{
+    public static class BubbleSort
+    {
+        public static int[] Sort(int[] nums)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+
+                for (var i = 0; i < nums.Length - 1; i++)
+                {
+                    if (nums[i] > nums[i + 1])
+                    {
+                        int temp = nums[i + 1];
+                        nums[i + 1] = nums[i];
+                        nums[i] = temp;
+                        swapped = true;
+                    }
+                }
+
+            } while (swapped == true);
+
+            return nums;
+        }
+    }    
+}
---
+++ PowerRecursive/PowerRecursive/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PowerRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare an array of integers that are not sorted
+            int[] nums = { 5, 10, 3, 2, 4 };
+
+            // Output the unsorted array to the console
+            Console.WriteLine("Before: 5, 10, 3, 2, 4");
+
+            nums = BubbleSort.Sort(nums);
+
+            Console.Write("After: ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0}, ", nums[i]);
+            }
+
+            // Use Console.ReadLine() in the event application was started with debugging.
+            Console.ReadLine();
+        }
+
+        static int Power(int baseNumber, int exponent)
+        {
+            if (exponent > 0)
+            {
+                exponent--;
+                return baseNumber * Power(baseNumber, exponent);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        static double CylinderVolume(float r, float h)
+        {
+            double pi = Math.PI;// 3.1444444;
+            double v = 0;
+
+            if (r > 0 && h > 0)
+            {
+                double area = pi * Math.Pow(r, 2);
+                v = area * h;
+            }
+
+            return Math.Round(v, 2);
+        }
+    }
+}
